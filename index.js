17d256205f228bbb5c886ab4cf6664adbbb7586b
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-06-01 21:24:47 +0530

    added folders

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
+
 const keys = require("./config/keys");
-const PORT = 5000;
+
 const app = express();
-app.get("/", (req, res) => {
-  res.send({ hello: `${keys.googleSecret}` });
-});
-app.listen(PORT, () => {
-  console.log("started server here");
-});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
