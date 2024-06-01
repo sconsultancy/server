c31e08a08f029aa68bb9a7973be53658b14bdd38
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-06-01 21:09:48 +0530

    added gitignore

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const keys = require("./config/keys");
 const PORT = 5000;
 const app = express();
 app.get("/", (req, res) => {
-  res.send({ hello: "there" });
+  res.send({ hello: `${keys.googleSecret}` });
 });
 app.listen(PORT, () => {
   console.log("started server here");
