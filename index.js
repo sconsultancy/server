4298f22414b82a6d00d9d53845ee5fd4c51992ca
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-06-01 21:42:34 +0530

    oauth and mongo added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
-
-const keys = require("./config/keys");
-
 const app = express();
-
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+const mongoose = require("mongoose");
+mongoose.connect(keys.mongoURI).then(() => {
+  console.log("mongo Connected");
+});
+require("./routes/authRoutes")(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,28 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+          console.log("user already exist");
+        } else {
+          // we don't have a user record with this ID, make a new record!
+          new User({ googleId: profile.id }).save();
+          console.log("Created user", profile.id);
+        }
+      });
+    }
+  )
+);
